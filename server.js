cce31cdad449e9133b0a82d1d935d25b1d12a725
Benjamin Leicht <b.leicht@gmx.de>
2020-10-07 16:24:37 +0200

    http methods "delete" + "post"

--- server.js
+++ server.js
@@ -26,4 +26,67 @@ app.get("/benutzer", async (req, res) => {
     res.json(rows);
 });
 
+app.post("/aufgaben", async (req, res) => {
+    const [
+      rows,
+    ] = await connection.execute(
+      "INSERT INTO aufgaben (aufgabenName, erstelldatum, abgabedatum, beschreibung, stand, benutzerID) VALUES (?, ?, ?, ?, ?, ?)",
+      [req.body.aufgabenName, req.body.erstelldatum, req.body.abgabedatum, req.body.beschreibung, req.body.stand, req.body.benutzerID]
+    );
+  
+    res.json({
+      id: rows.insertId,
+      aufgabenName: req.body.aufgabenName,
+      erstelldatum: req.body.erstelldatum,
+      abgabedatum: req.body.abgabedatum,
+      beschreibung: req.body.beschreibung,
+      stand: req.body.stand,
+      benutzerID: req.body.benutzerID,
+    });
+  });
+
+app.post("/benutzer", async (req, res) => {
+    const [
+      rows,
+    ] = await connection.execute(
+      "INSERT INTO benutzer (benutzerName, email, passwort) VALUES (?, ?, ?)",
+      [req.body.benutzerName, req.body.email, req.body.passwort]
+    );
+  
+    res.json({
+      id: rows.insertId,
+      benutzerName: req.body.benutzerName,
+      email: req.body.email,
+      passwort: req.body.passwort,
+    });
+  });
+
+app.delete("/aufgaben/:id", async (req, res) => {
+    console.log(req.params.id);
+  
+    const [rows] = await connection.execute("DELETE FROM aufgaben WHERE id = ?", [
+      req.params.id,
+    ]);
+  
+    if (rows.affectedRows === 1) {
+      res.status(200).send();
+    } else {
+      res.status(404).send();
+    }
+});
+
+app.delete("/benutzer/:id", async (req, res) => {
+    console.log(req.params.id);
+  
+    const [rows] = await connection.execute("DELETE FROM benutzer WHERE id = ?", [
+      req.params.id,
+    ]);
+  
+    if (rows.affectedRows === 1) {
+      res.status(200).send();
+    } else {
+      res.status(404).send();
+    }
+});
+
 app.listen(55);
