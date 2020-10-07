ccc92eb5c1f16913b5c155e055b1f56b01a966bb
Matthias Gelszinnis <matthias.gelszinnis@gmail.com>
2020-10-07 19:07:11 +0200

    weitere Testdaten hinzugefügt

--- database.sql
+++ database.sql
@@ -8,8 +8,11 @@ CREATE TABLE IF NOT EXISTS benutzer (id int NOT NULL AUTO_INCREMENT, benutzerNam
 
 INSERT INTO benutzer (benutzerName, email, passwort) VALUES ('Müller', 'test@gmail.com', 'test123');
 INSERT INTO benutzer (benutzerName, email, passwort) VALUES ('Schmidt', 'test2@gmail.com', 'passwort123');
+INSERT INTO benutzer (benutzerName, email, passwort) VALUES ('Huber', 'test3@gmail.com', 'tollespasswort');
 
 CREATE TABLE IF NOT EXISTS aufgaben (id int NOT NULL AUTO_INCREMENT, aufgabenName char(30), erstelldatum char(10), abgabedatum char(10), beschreibung char(60), stand int(1), benutzerID int(10), constraint aufgabekey PRIMARY KEY (id), constraint Benutzerzuordnung foreign key key1 (benutzerID) references benutzer (id) on delete set null);
 
-INSERT INTO aufgaben (aufgabenName, erstelldatum, abgabedatum, beschreibung, stand, benutzerID) VALUES ('testaufgabe', '2020-01-04', '2020-01-08', 'eine Aufgabe zum Testen', 3, 2);
+INSERT INTO aufgaben (aufgabenName, erstelldatum, abgabedatum, beschreibung, stand, benutzerID) VALUES ('testaufgabe', '2020-01-04', '2020-11-08', 'eine Aufgabe zum Testen', 3, 2);
+INSERT INTO aufgaben (aufgabenName, erstelldatum, abgabedatum, beschreibung, stand, benutzerID) VALUES ('JSTest', '2020-03-04', '2020-11-08', 'eine Aufgabe zum Testen', 0, 1);
+INSERT INTO aufgaben (aufgabenName, erstelldatum, abgabedatum, beschreibung, stand, benutzerID) VALUES ('Expresstest', '1999-01-04', '2021-11-08', 'eine Aufgabe zum Testen', 2, 3);
 
