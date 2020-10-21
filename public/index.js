8f2e7e8ac8c4b1e8887362c2cd3e6cddcd8a2a44
Marvin Kickel <marvin.kickel@gmx.de>
2020-10-21 19:18:57 +0200

    Login

---
+++ public/index.js
@@ -0,0 +1,28 @@
+/*const form = document.querySelector(".form-signin");
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const email = $('#inputEmail').val();
+    const passwort = $('#inputPassword').val();
+
+   const values = {
+        email: email,
+        passwort: passwort
+        
+   };
+
+   fetch("/login", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+            "content-type": "application/json",
+        },
+    }).then((res) => {
+        if (res.ok) {
+            location.href = '/main.html';
+        } else {
+            alert("Login fehlgeschlagen");
+        }
+    }); 
+}); */
